3cf737719c498e537da5216ea0c8b65cc75e5930
shubham23066 <shubham23066@gmail.com>
2024-08-12 18:46:37 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "cloudcore007"
+    bucket = "cloudcore0071"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "eu-central-1"
   }
 
   required_version = ">= 1.6.3"
